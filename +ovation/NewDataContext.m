81befa1e15b125b84f15dc6aee5041a8ac40a3da
Barry Wark <barrywark@gmail.com>
2013-06-05 14:26:29 -0400

    Removed hdf5 and added logindlg
    logindlg is BSD license

--- +ovation/NewDataContext.m
+++ +ovation/NewDataContext.m
@@ -1,43 +1,38 @@
-function context = NewDataContext(connection_file, varargin)
+function context = NewDataContext(userEmail, password)
 % Creates a new ovation.DataContext with a connection to the specified
 % connection file. Prompts for the user password.
 %
-%  context = NewDataContext(path_to_connection_file, [username]);
+%  context = NewDataContext(email, [password]);
 %
-%    connection_file: path to Ovation connection file
-%    username (optional): Ovation username for authenticating the 
+%    email (optional): ovation.io user email
+%    password (optional): ovation.io password for authenticating the 
 %                         new DataContext
 %
 
 % Copyright (c) 2012 Physion Consulting LLC
 
-
-    error(nargchk(1,2, nargin)); %#ok<NCHKN>
+    import ovation.util.*;
     
-    if(nargin < 2)
-        username = '';
-    else
-        username = varargin{1};
-    end
+    narginchk(0,2);
     
-	import ovation.*;
-    import com.physion.ovation.ui.*
-    
-    if(isempty(strfind(connection_file, '::')))
-        f = java.io.File(connection_file);
-        if(~f.isAbsolute())
-            fullPath = fullfile(pwd(), connection_file);
-            f = java.io.File(fullPath);
-            connection_file = f.getCanonicalPath();
+    if(nargin == 0)
+        [userEmail, password] = logindlg('Title', 'ovation.io');
+        if(isempty(userEmail) || isempty(password))
+            context = [];
+            return
+        end
+    elseif(nargin == 1)
+        password = logindlg('Title', ['ovation.io (' userEmail ')'], 'Password', 'only');
+        if(isempty(password))
+            context = [];
+            return
         end
     end
     
-    model = DatabaseCredentialsModel(username);
-    if(DatabaseCredentialsDialog.showCredentialsDialog(model))
-        context = Ovation.connect(connection_file,...
-            model.getUsername(),...
-            java.lang.String(model.getPassword()));
-    else
-        context = [];
-    end
+	import us.physion.ovation.api.*
+    
+    disp('Authenticating...');
+    dsc = Ovation.connect(userEmail, password);
+    
+    context = dsc.getContext();
 end
--- test/TestMatlabSuite.m
+++ test/TestMatlabSuite.m
@@ -4,10 +4,10 @@ classdef TestMatlabSuite < TestCase
     
     properties
         context
-        connection_file
-        existing_prefs
-        ovationPreferenceGroup
-        testManager
+        localStack
+        databaseName
+        userIdentity
+        userPassword
         dsc
     end
     
@@ -15,70 +15,34 @@ classdef TestMatlabSuite < TestCase
         function self = TestMatlabSuite(name)
             
             self = self@TestCase(name);
-            
-            self.ovationPreferenceGroup = 'ovation';
-            self.existing_prefs = getpref(self.ovationPreferenceGroup);
-        end
-        
-        function testFdName = federationName(self)
-            testFdName = [class(self)  '.' self.MethodName];
         end
         
         function setUp(self)
             
-            import ovation.*;
-            import ovation.database.*;
-            import ovation.test.*;
+            import us.physion.ovation.test.util.*;
+            import us.physion.ovation.api.*;
             
-            if isempty(getenv('OVATION_CONNECTION_FILE'))
-                system('mkdir -p ovation');
-                setenv('OVATION_CONNECTION_FILE', fullfile(pwd(), 'ovation/matlab_test.connection'));
-            end
-            
-            if isempty(getenv('OVATION_TEST_FDID'))
-                setenv('OVATION_TEST_FDID', '13');
-            end
-            
-            if isempty(getenv('OVATION_LOCK_SERVER'))
-                [~,result] = system('hostname');
-                result = result(1:end-1); % remove trailing newline
-                setenv('OVATION_LOCK_SERVER', result);
-            end
-            
-            
-            self.connection_file = getenv('OVATION_CONNECTION_FILE');
-            
-            if ~exist(self.connection_file, 'file')
-                DatabaseManager.createLocalDatabase(self.connection_file,...
-                    getenv('OVATION_LOCK_SERVER'),...
-                    str2double(getenv('OVATION_TEST_FDID')));
-            end
-            
-            
-            institution = 'Institution';
-            lab = 'Lab';
-            licenseCode = 'crS9RjS6wJgmZkJZ1WRbdEtIIwynAVmqFwrooGgsM7ytyR+wCD3xpjJEENey+b0GVVEgib++HAKh94LuvLQXQ2lL2UCUo75xJwVLL3wmd21WbumQqKzZk9p6fkHCVoiSxgon+2RaGA75ckKNmUVTeIBn+QkalKCg9p1P7FbWqH3diXlAOKND2mwjI8V4unq7aaKEUuCgdU9V/BjFBkoytG8FzyBCNn+cBUNTByYy7RxYxH37xECZJ6/hG/vP4QjKpks9cu3yQL9QjXBQIizrzini0eQj62j+QzCSf0oQg8KdIeZHuU+ZSZZ1pUHLYiOiQWaOL9cVPxqMzh5Q/Zvu6Q==';
-            
-            self.patchLicensePreferences(institution, lab, licenseCode);
-            
-            self.testManager = [];
+
             try
-                self.testManager = TestManager(self.connection_file,...
-                    institution,...
-                    lab,...
-                    licenseCode,...
-                    'TestUser',...
-                    'TestPassword');
+                self.databaseName = 'matlab_test-email-com';
+                self.userIdentity = 'matlab_test@email.com'
+                self.userPassword = 'matlab'
+
+                self.localStack = TestUtils().makeLocalStack(OvationApiModule(), ...
+                    self.databaseName, ...
+                    self.userIdentity, ...
+                    self.userPassword);
                 
                 
-                self.dsc = self.testManager.setupDatabase();
+                self.dsc = self.localStack.getAuthenticatedDataStoreCoordinator();
                 
                 
                 self.context = self.dsc.getContext();
+
             catch ex
                 disp(ex.message)
-                if ~isempty(self.testManager)
-                    self.testManager.tearDownDatabase();
+                if ~isempty(self.localStack)
+                    self.localStack.cleanUp();
                 end
             end
             
@@ -89,49 +53,11 @@ classdef TestMatlabSuite < TestCase
         
         function tearDown(self)
             try
-                self.testManager.tearDownDatabase();
+                self.localStack.cleanUp();
             catch ex
                 assert(false, ex.message);
             end
-            
-            self.restoreLicensePreferences();
            
         end
-        
-        function patchLicensePreferences(self, institution, lab, licenseCode)
-            if ~strcmp('', getpref(self.ovationPreferenceGroup, 'ovation_license_institution'))
-                self.existing_prefs.ovation_license_institution = getpref(self.ovationPreferenceGroup, 'ovation_license_institution');
-            end
-            
-            if ~strcmp('', getpref(self.ovationPreferenceGroup, 'ovation_license_lab'))
-                self.existing_prefs.ovation_license_lab = getpref(self.ovationPreferenceGroup, 'ovation_license_lab');
-            end
-            
-            if ~strcmp('', getpref(self.ovationPreferenceGroup, 'ovation_license_licenseText'))
-                self.existing_prefs.ovation_license_licenseText = getpref(self.ovationPreferenceGroup, 'ovation_license_licenseText');
-            end
-            
-            setpref(self.ovationPreferenceGroup, 'ovation_license_institution', institution);
-            setpref(self.ovationPreferenceGroup, 'ovation_license_lab', lab);
-            setpref(self.ovationPreferenceGroup, 'ovation_license_licenseText', licenseCode);
-        end
-        
-        function restoreLicensePreferences(self)
-            % restore matlab prefs
-            if(~isempty(self.existing_prefs))
-                if(isfield(self.existing_prefs, 'ovation_license_licenseText'))
-                    setpref(self.ovationPreferenceGroup,...
-                        'ovation_license_institution',...
-                        self.existing_prefs.ovation_ovation_license_institution);
-                    setpref(self.ovationPreferenceGroup,...
-                        'ovation_license_lab',...
-                        self.existing_prefs.ovation_license_lab);
-                    setpref(self.ovationPreferenceGroup,...
-                        'ovation_license_licenseText',...
-                        self.existing_prefs.ovation_license_licenseText);
-                end
-            end
-        end
-        
     end
 end
