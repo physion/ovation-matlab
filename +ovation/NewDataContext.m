f33aa5f8f91bd1e00bf910e74a03841854f85926
Barry Wark <barrywark@gmail.com>
2013-12-11 15:48:36 -0500

    Working download to support directory
    Everything seems to work, except for one minor detail… ;-)
    DSC authenticates, but connectMethod.invoke() doesn’t return anything.
    Argh!

--- +ovation/NewDataContext.m
+++ +ovation/NewDataContext.m
@@ -16,16 +16,6 @@ function context = NewDataContext(userEmail, password)
     
     narginchk(0,2);
     
-    
-    classLoader = ovation_classloader(fullfile(fileparts(which(mfilename('fullpath'))), 'ovation.jar'));
-    ovationClass = classLoader.loadClass('us.physion.ovation.api.Ovation');
-    strClass = classLoader.loadClass('java.lang.String');
-    connectMethod = ovationClass.getMethod('connect', [strClass, strClass]);
-    versionMethod = ovationClass.getMethod('getVersion', []);
-    
-    currentThread = Thread.currentThread();
-    currentThread.setContextClassLoader(ovationClass.getClassLoader());
-    
     if(nargin == 0)
         [userEmail, password] = logindlg('Title', 'ovation.io');
         if(isempty(userEmail) || isempty(password))
@@ -40,10 +30,78 @@ function context = NewDataContext(userEmail, password)
         end
     end
     
+    jarName = 'ovation.jar';
+    jarEtag = 'ovation.jar.etag';
+    homeFolder = char(java.lang.System.getProperty('user.home'));
+    if(ismac())
+        jarFolder = fullfile(homeFolder, 'Library', 'Application Support', 'us.physion.ovation', 'matlab');
+    elseif(ispc())
+        jarFolder = fullfile(homeFolder, 'AppData', 'Local', 'ovation', 'matlab');
+    elseif(isunix())
+        jarFolder = fullfile(homeFolder, '.ovation', 'matlab');
+    else
+        error('Unsupported platform');
+    end
+    
+    if(~exist(jarFolder, 'dir'))
+        mkdir(jarFolder);
+    end
+    
+    jarPath = fullfile(jarFolder, jarName);
+    etagPath = fullfile(jarFolder, jarEtag);
+    
+    [r,status] = urlread('https://ovation.io/api/v1/sessions', ...
+        'POST', {'email', userEmail, 'password', password});
+    
+    if(status)
+        r = ovation.util.parse_json(r);
+        session = r{1};
+        
+        api_key = session.api_key;
+        
+        [r,status] = urlread('https://ovation.io/api/v1/downloads/Ovation%20Matlab%20API%20Core',...
+            'Authentication','Basic',...
+            'Username', api_key,...
+            'Password', api_key);
+        
+        if(status)
+           r = ovation.util.parse_json(r);
+           download_info = r{1};
+           
+           if(~exist(etagPath, 'file') || ...
+                   ~strcmp(ovation.util.read_lines(etagPath),...
+                   download_info.etag))
+              
+               disp('Downloading Ovation Core API...');
+               urlwrite(download_info.url, jarPath);
+               
+           end
+        end
+    end
+    
+    
+    if(~exist(jarPath, 'file'))
+        error('Ovation Matlab Core API JAR is not available'); %TODO a better error message.
+    end
+    
+    disp('Loading Ovation libraries...');
+    
+    classLoader = ovation_classloader(fullfile(fileparts(which(mfilename('fullpath'))), 'ovation.jar'));
+    
+    currentThread = Thread.currentThread();
+    currentThread.setContextClassLoader(classLoader);
+    
+    ovationClass = classLoader.loadClass('us.physion.ovation.api.Ovation');
+    strClass = classLoader.loadClass('java.lang.String');
+    connectMethod = ovationClass.getMethod('connect', [strClass, strClass]);
+    versionMethod = ovationClass.getMethod('getVersion', []);
+    
+    
+    
 	import us.physion.ovation.api.*
     
     disp(['Ovation Matlab Core API version ' char(versionMethod.invoke([], []))]);
-    
+    disp(' ');
     disp('Authenticating...');
     
     args = javaArray('java.lang.String', 2);
