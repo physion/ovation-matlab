3b2d1f5d046bf33d368b72fb179961788befac24
Barry Wark <barrywark@gmail.com>
2013-12-13 13:02:18 -0500

    Simplify urlread_auth

--- +ovation/+util/urlread_auth.m
+++ +ovation/+util/urlread_auth.m
@@ -1,31 +1,30 @@
-function [s,info] = urlread_auth(url, user, password)
+function [s,info] = urlread_auth(javaUrl, user, password)
     %URLREAD_AUTH Like URLREAD, with basic authentication
     %
     % [s,info] = urlread_auth(url, user, password)
     %
-    % Returns bytes. Convert to char if you're retrieving text.
     %
     % Examples:
-    % sampleUrl = 'http://browserspy.dk/password-ok.php';
+    % sampleUrl = 'http://ovation.io/123';
     % [s,info] = urlread_auth(sampleUrl, 'test', 'test');
-    % txt = char(s)
     
     % Originally from http://stackoverflow.com/a/1323535/2140
     
-    % Matlab's urlread() doesn't do HTTP Request params, so work directly with Java
-    jUrl = java.net.URL(url);
-    conn = jUrl.openConnection();
-    conn.setRequestProperty('Authorization', ['Basic ' base64encode([user ':' password])]);
+    
+    javaUrl = java.net.URL(javaUrl);
+    conn = javaUrl.openConnection();
+    
+    encoder = sun.misc.BASE64Encoder();
+    authString = [user ':' password];
+    auth = char(encoder.encode(java.lang.String(authString).getBytes()));
+    conn.setRequestProperty('Authorization', ['Basic ' auth]);\
+    
     conn.connect();
+    
     info.status = conn.getResponseCode();
     info.errMsg = char(readstream(conn.getErrorStream()));
-    s = readstream(conn.getInputStream());
-end
-
-function out = base64encode(str)
-    % Uses Sun-specific class, but we know that is the JVM Matlab ships with
-    encoder = sun.misc.BASE64Encoder();
-    out = char(encoder.encode(java.lang.String(str).getBytes()));
+    
+    s = char(readstream(conn.getInputStream()));
 end
 
 %%
@@ -33,12 +32,12 @@ function out = readstream(inStream)
     %READSTREAM Read all bytes from stream to uint8
     try
         import com.mathworks.mlwidgets.io.InterruptibleStreamCopier;
-        byteStream = java.io.ByteArrayOutputStream();
+        bos = java.io.ByteArrayOutputStream();
         isc = InterruptibleStreamCopier.getInterruptibleStreamCopier();
-        isc.copyStream(inStream, byteStream);
+        isc.copyStream(inStream, bos);
         inStream.close();
-        byteStream.close();
-        out = typecast(byteStream.toByteArray', 'uint8'); %'
+        bos.close();
+        out = typecast(bos.toByteArray', 'uint8'); %'
     catch err
         out = []; %HACK: quash
     end
