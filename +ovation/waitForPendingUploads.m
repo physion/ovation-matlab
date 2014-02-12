ba944d8d2c4f15584c88415f11d2ae6aae46b213
Barry Wark <barrywark@gmail.com>
2014-02-12 10:45:25 -0500

    Corrects waitForPendingUploads for timeout

--- +ovation/waitForPendingUploads.m
+++ +ovation/waitForPendingUploads.m
@@ -3,6 +3,20 @@
 function waitForPendingUploads(context)
     fs = context.getFileService();
     while(fs.hasPendingUploads())
-        fs.waitForPendingUploads(60, java.util.concurrent.TimeUnit.MINUTES);
+        try
+            fs.waitForPendingUploads(60, java.util.concurrent.TimeUnit.MINUTES);
+        catch e
+            timeout = false;
+            if(isa(e, 'matlab.exception.JavaException'))
+                ex = e.ExceptionObject;
+                if(isa(ex, 'java.util.concurrent.TimeoutException'))
+                    timeout = true;
+                end
+            end
+            
+            if(~timeout)
+                rethrow(e);
+            end
+        end
     end
 end
