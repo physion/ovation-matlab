ed5b15f56cc62f907f639bb95e378fa9511619a4
Barry Wark <barrywark@gmail.com>
2014-06-11 12:07:35 -0400

    Adds test for Epoch AR parity

--- test/TestAnalysisRecordInsertion.m
+++ test/TestAnalysisRecordInsertion.m
@@ -7,7 +7,7 @@ classdef TestAnalysisRecordInsertion < MatlabTestCase
             self = self@MatlabTestCase(name);
         end
         
-        function testInsertsAnalysisRecord(self)
+        function testInsertsProjectAnalysisRecord(self)
             import ovation.*
             
             data1 = [1,2,3];
@@ -32,6 +32,50 @@ classdef TestAnalysisRecordInsertion < MatlabTestCase
             params.k2 = 3;
             
             ar2 = ovation.addAnalysis(proj,... % where
+                name,...
+                inputs,...
+                params,...
+                outputs);
+            
+            assert(~isempty(ar2));
+            assert(ar2.getName().equals(name));
+        end
+        
+        function testInsertsEpochAnalysisRecord(self)
+            import ovation.*
+            
+            data1 = [1,2,3];
+            
+            nd = us.physion.ovation.values.NumericData();
+            nd.addData('data1', data1, 'units', 1, 'Hz');
+            
+            proj = self.context.insertProject('test', 'test', ovation.datetime());
+            exp = proj.insertExperiment('test', ovation.datetime());
+            epoch = exp.insertEpoch(ovation.datetime(),...
+                ovation.datetime(),...
+                [],...
+                ovation.struct2map(struct()),...
+                ovation.struct2map(struct()));
+            
+            nm = epoch.insertNumericMeasurement('name',...
+                ovation.array2set({}),...
+                ovation.array2set({}),...
+                nd);
+                
+            
+            fname = tempname;
+            fid = fopen(fname, 'w');
+            fprintf(fid, 'hello world');
+            fclose(fid);
+            
+            inputs = {nm};
+            outputs.text_file = fname;
+            name = 'my reocrd name';
+            
+            params.k1 = 'v1';
+            params.k2 = 3;
+            
+            ar2 = ovation.addAnalysis(epoch,... % where
                 name,...
                 inputs,...
                 params,...
